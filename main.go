78114f133a9e29584f8eca8bda3aa95a08bffecb
Rob Greene <robgreene@gmail.com>
2020-02-16 14:06:41 -0600

    Packet proxy works but source IP is not kept so target is incorrect.

--- main.go
+++ main.go
@@ -10,8 +10,29 @@ import (
 
 var count = 0
 var mutex = sync.Mutex{}
+var sourceCIDR *net.IPNet
+var sourceAddr net.Addr
+var targetCIDR *net.IPNet
+var targetAddr *net.UDPAddr
 
 func main() {
+	var err error
+	_, sourceCIDR, err = net.ParseCIDR("192.168.123.0/24")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, targetCIDR, err = net.ParseCIDR("192.168.5.0/24")
+	if err != nil {
+		log.Fatal(err)
+	}
+	targetAddr, err = net.ResolveUDPAddr("udp", "192.168.5.255:65001")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Source CIDR = %v\n", sourceCIDR)
+	fmt.Printf("Target CIDR = %v\n", targetCIDR)
+	fmt.Printf("Target Address = %v\n", targetAddr)
 	fmt.Println("Starting...")
 
 	// listen to incoming udp packets
@@ -36,6 +57,24 @@ func serve(pc net.PacketConn, addr net.Addr, buf []byte) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	fmt.Printf("packet #%d, network=%s, addr=%s\nData:\n%s\n", count, addr.Network(), addr.String(), hex.Dump(buf))
+	fmt.Printf("packet #%d, network=%s, addr=%s\nData:\n%s", count, addr.Network(), addr.String(), hex.Dump(buf))
 	count++
+
+	host, _, err := net.SplitHostPort(addr.String())
+	if err != nil {
+		log.Fatal(err)
+	}
+	ip := net.ParseIP(host)
+	fmt.Printf("Parsed IP = %v\n", ip)
+
+	if sourceCIDR.Contains(ip) {
+		sourceAddr = addr
+		pc.WriteTo(buf, targetAddr)
+		fmt.Printf("Relayed to %s\n\n", targetAddr)
+	} else if targetCIDR.Contains(ip) {
+		pc.WriteTo(buf, sourceAddr)
+		fmt.Printf("Relayed to %s\n\n", sourceAddr)
+	} else {
+		fmt.Printf("Does not match CIDR's given; skipping...\n\n")
+	}
 }
