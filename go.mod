aa538a14ba537793e9b949e6636cb6923fa94d05
Rob Greene <robgreene@gmail.com>
2020-02-19 20:09:08 -0600

    Trying NewRawConn; all hard-coded values for the time being.

---
+++ go.mod
@@ -0,0 +1,8 @@
+module goudp
+
+go 1.13
+
+require (
+	github.com/google/gopacket v1.1.17
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
+)
--- main.go
+++ main.go
@@ -5,15 +5,24 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 	"sync"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+
+	"golang.org/x/net/ipv4"
 )
 
 var count = 0
 var mutex = sync.Mutex{}
 var sourceCIDR *net.IPNet
 var sourceAddr net.Addr
+var sourceIP net.IP
+var sourceIfIndex int
 var targetCIDR *net.IPNet
 var targetAddr *net.UDPAddr
+var ignoreIPs []string
 
 func main() {
 	var err error
@@ -29,6 +38,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	ignoreIPs = strings.Split("192.168.5.254", ",")
 
 	fmt.Printf("Source CIDR = %v\n", sourceCIDR)
 	fmt.Printf("Target CIDR = %v\n", targetCIDR)
@@ -36,45 +46,127 @@ func main() {
 	fmt.Println("Starting...")
 
 	// listen to incoming udp packets
-	pc, err := net.ListenPacket("udp", ":65001")
+	netpc, err := net.ListenPacket("ip4:udp", "0.0.0.0")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer pc.Close()
+	defer netpc.Close()
 
+	pc, err := ipv4.NewRawConn(netpc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pc.SetControlMessage(ipv4.FlagDst, true)
+	pc.SetControlMessage(ipv4.FlagSrc, true)
+
+	//for i := 0; i < 10; i++ {
 	for {
 		buf := make([]byte, 1024)
-		n, addr, err := pc.ReadFrom(buf)
+		h, p, cm, err := pc.ReadFrom(buf)
 		if err != nil {
 			continue
 		}
-		go serve(pc, addr, buf[:n])
+		go serve(pc, h, p, cm, buf[:h.TotalLen])
 	}
 
 }
 
-func serve(pc net.PacketConn, addr net.Addr, buf []byte) {
+func serve(pc *ipv4.RawConn, h *ipv4.Header, p []byte, cm *ipv4.ControlMessage, buf []byte) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	fmt.Printf("packet #%d, network=%s, addr=%s\nData:\n%s", count, addr.Network(), addr.String(), hex.Dump(buf))
-	count++
+	if contains(ignoreIPs, h.Src.String()) {
+		fmt.Printf("Ignoring %s\n\n", h.Src.String())
+		return
+	}
 
-	host, _, err := net.SplitHostPort(addr.String())
+	packet := layers.UDP{}
+	err := packet.DecodeFromBytes(p, gopacket.NilDecodeFeedback)
 	if err != nil {
 		log.Fatal(err)
 	}
-	ip := net.ParseIP(host)
-	fmt.Printf("Parsed IP = %v\n", ip)
-
-	if sourceCIDR.Contains(ip) {
-		sourceAddr = addr
-		pc.WriteTo(buf, targetAddr)
-		fmt.Printf("Relayed to %s\n\n", targetAddr)
-	} else if targetCIDR.Contains(ip) {
-		pc.WriteTo(buf, sourceAddr)
-		fmt.Printf("Relayed to %s\n\n", sourceAddr)
-	} else {
-		fmt.Printf("Does not match CIDR's given; skipping...\n\n")
+	if packet.DstPort != 65001 {
+		if packet.SrcPort == 65001 {
+			fmt.Printf("RETURN packet, header=%v, control=%v, udp=%v\nPayload:\n%sData:\n%s\nUDP Payload:\n%s",
+				h, cm, packet, hex.Dump(p), hex.Dump(buf), hex.Dump(packet.Payload))
+		}
+		return
 	}
+
+	fmt.Printf("packet #%d, header=%v, control=%v, udp=%v\nPayload:\n%sData:\n%s\nUDP Payload:\n%s",
+		count, h, cm, packet, hex.Dump(p), hex.Dump(buf), hex.Dump(packet.Payload))
+	count++
+
+	bcast, err := net.ResolveIPAddr("ip", "192.168.5.117")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Yeah, hack
+	buf[16] = bcast.IP.To4()[0]
+	buf[17] = bcast.IP.To4()[1]
+	buf[18] = bcast.IP.To4()[2]
+	buf[19] = bcast.IP.To4()[3]
+	// h.Dst = bcast.IP.To4()
+	// newpacket, err := h.Marshal()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	fmt.Printf("Redirecting to %v\nNew Packet:\n%s\n\n", bcast, hex.Dump(buf))
+	_, err = pc.WriteToIP(buf, bcast)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// host, _, err := net.SplitHostPort(addr.String())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// ip := net.ParseIP(host)
+	// fmt.Printf("Parsed IP = %v\n", ip)
+
+	// if contains(ignoreIPs, host) {
+	// 	fmt.Printf("Ignoring %s\n\n", host)
+	// } else if sourceCIDR.Contains(ip) {
+	// 	sourceAddr = addr
+	// 	sourceIP = ip
+	// 	sourceIfIndex = cm.IfIndex
+	// 	pc.WriteTo(buf, nil, targetAddr)
+	// 	fmt.Printf("Relayed to %s\n\n", targetAddr)
+	// } else if targetCIDR.Contains(ip) && sourceAddr != nil {
+	// 	relaycm := &ipv4.ControlMessage{
+	// 		//IfIndex: sourceIfIndex,
+	// 		//Src: cm.Src,
+	// 		Src: net.ParseIP("192.168.5.254"),
+	// 		//Dst: sourceIP,
+	// 		Dst: net.ParseIP("192.168.123.11"),
+	// 		TTL: 64,
+	// 	}
+	// 	_, port, err := net.SplitHostPort(sourceAddr.String())
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	theaddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("192.168.123.11:%s", port))
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Printf("WriteTo(buf, %v, %v)\n", relaycm, theaddr)
+	// 	n, err := pc.WriteTo(buf, relaycm, theaddr)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Printf("Relayed %d bytes to %s (control=%v)\n\n", n, theaddr, relaycm)
+	// 	// pc.WriteTo(buf, nil, sourceAddr)
+	// 	// fmt.Printf("Relayed to %s\n\n", sourceAddr)
+	// } else {
+	// 	fmt.Printf("Does not match CIDR's given; skipping...\n\n")
+	// }
+}
+
+func contains(a []string, s string) bool {
+	for _, v := range a {
+		if v == s {
+			return true
+		}
+	}
+	return false
 }
