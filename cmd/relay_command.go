8913beffb7e15e8a0ed225a3a3313a4773ddd170
Rob Greene <robgreene@gmail.com>
2020-02-22 11:24:29 -0600

    Cleaning up a bit.

--- cmd/relay_command.go
+++ cmd/relay_command.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"strings"
 	"sync"
 
 	"hdhomerun-discover-relay/packet"
@@ -16,11 +15,6 @@ import (
 var count = 0
 var mutex = sync.Mutex{}
 var sourceCIDR *net.IPNet
-var sourceAddr net.Addr
-var sourceIP net.IP
-var sourceIfIndex int
-var targetCIDR *net.IPNet
-var ignoreIPs []string
 
 type RelayCommand struct {
 	Args struct {
@@ -34,14 +28,8 @@ func (cmd RelayCommand) Execute(args []string) error {
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, targetCIDR, err = net.ParseCIDR("192.168.5.0/24")
-	if err != nil {
-		log.Fatal(err)
-	}
-	ignoreIPs = strings.Split("192.168.5.254", ",")
 
 	fmt.Printf("Source CIDR = %v\n", sourceCIDR)
-	fmt.Printf("Target CIDR = %v\n", targetCIDR)
 	fmt.Println("Starting...")
 
 	// listen to incoming udp packets (all interfaces)
@@ -72,8 +60,8 @@ func serve(pc *ipv4.RawConn, h *ipv4.Header, p []byte, cm *ipv4.ControlMessage, 
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	if contains(ignoreIPs, h.Src.String()) {
-		fmt.Printf("Ignoring %s\n\n", h.Src.String())
+	if !sourceCIDR.Contains(h.Src) {
+		fmt.Printf("Ignoring %s\n", h.Src.String())
 		return
 	}
 
@@ -104,50 +92,6 @@ func serve(pc *ipv4.RawConn, h *ipv4.Header, p []byte, cm *ipv4.ControlMessage, 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// host, _, err := net.SplitHostPort(addr.String())
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// ip := net.ParseIP(host)
-	// fmt.Printf("Parsed IP = %v\n", ip)
-
-	// if contains(ignoreIPs, host) {
-	// 	fmt.Printf("Ignoring %s\n\n", host)
-	// } else if sourceCIDR.Contains(ip) {
-	// 	sourceAddr = addr
-	// 	sourceIP = ip
-	// 	sourceIfIndex = cm.IfIndex
-	// 	pc.WriteTo(buf, nil, targetAddr)
-	// 	fmt.Printf("Relayed to %s\n\n", targetAddr)
-	// } else if targetCIDR.Contains(ip) && sourceAddr != nil {
-	// 	relaycm := &ipv4.ControlMessage{
-	// 		//IfIndex: sourceIfIndex,
-	// 		//Src: cm.Src,
-	// 		Src: net.ParseIP("192.168.5.254"),
-	// 		//Dst: sourceIP,
-	// 		Dst: net.ParseIP("192.168.123.11"),
-	// 		TTL: 64,
-	// 	}
-	// 	_, port, err := net.SplitHostPort(sourceAddr.String())
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	theaddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("192.168.123.11:%s", port))
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Printf("WriteTo(buf, %v, %v)\n", relaycm, theaddr)
-	// 	n, err := pc.WriteTo(buf, relaycm, theaddr)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Printf("Relayed %d bytes to %s (control=%v)\n\n", n, theaddr, relaycm)
-	// 	// pc.WriteTo(buf, nil, sourceAddr)
-	// 	// fmt.Printf("Relayed to %s\n\n", sourceAddr)
-	// } else {
-	// 	fmt.Printf("Does not match CIDR's given; skipping...\n\n")
-	// }
 }
 
 func contains(a []string, s string) bool {
